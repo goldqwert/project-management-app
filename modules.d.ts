c4e33398c6566584d7c03f3103d416930577a009
ksy336 <ksuoxy773@gmail.com>
2022-05-03 16:46:26 +0400

    feat: add webpack.config.js, tsconfig.json, eslint, prettier and changed package.json

---
+++ modules.d.ts
@@ -0,0 +1,11 @@
+declare module '*.tsx';
+declare module '*.ts';
+declare module '*.jsx';
+declare module '*.js';
+declare module 'redux-promise';
+declare module 'react';
+declare module 'react-redux';
+declare module 'react-dom';
+declare module 'socket.io-client';
+declare module 'react-router-dom';
+declare module '*.scss';
